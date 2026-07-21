bf51749881deb42dfc808121de39a7a9a36a529b
Hyson Peter Razon David <hysonpeterdavid@gmail.com>
2026-07-21 13:00:07 +1000

    Add sql files 1

---
+++ 04 SQL Files/S2T6- 1. Create Database.sql
@@ -0,0 +1,9 @@
+# CREATE DATABASE IN SQL 
+# CREATE DATABASE StreamFlixDB; # Remove '#' if first time to create DB
+USE StreamFlixDB; #Switch to DB selected
+
+
+## VERIFY: Ways to check and switch DB.
+SELECT DATABASE(); # Check which database is currently selected
+SHOW databases; # Show all all databases in MySQL
+
